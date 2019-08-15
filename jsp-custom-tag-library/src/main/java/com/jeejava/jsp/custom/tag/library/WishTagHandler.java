4d801347c09d2354fbfda943b97eef23a110eb45
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-08-15 08:37:06 +0530

    Add files via upload

---
+++ jsp-custom-tag-library/src/main/java/com/jeejava/jsp/custom/tag/library/WishTagHandler.java
@@ -0,0 +1,34 @@
+package com.jeejava.jsp.custom.tag.library;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class WishTagHandler extends TagSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	@Override
+	public int doStartTag() throws JspException {
+		JspWriter jspWriter = pageContext.getOut();
+		try {
+			jspWriter.println("Hello " + name);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return SKIP_BODY;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ jsp-custom-tag-library/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@taglib prefix="test" uri="/WEB-INF/tld/wishTagHandler.tld"%>
+<html>
+<head>
+<title>JSP Custom Tag Library Example</title>
+</head>
+<body>
+	<font color="blue">
+		<test:wish name="Soumitra" />
+	</font>
+</body>
+</html>
